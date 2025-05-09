eadd0e296069f4af91b61dd1318faa18ade62ed5
Atish1910 <atishkamble398@gmail.com>
2025-05-09 20:32:05 +0530

    40

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -20,7 +20,7 @@ function Login({ setIsLoggedIn, setLoggedInUser }) {
     debugger
     try {
       // Get APi to check where User is Present in DB or not
-      const allUsersRes = await axios.get("https://ballr-mern-ashish.onrender.com/getAllusers");
+      const allUsersRes = await axios.get("http://localhost:4000/getAllusers");
       const users = allUsersRes.data.data || [];
       const emailExists = users.some(
         (user) => 
